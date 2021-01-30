8e9ef5349d94909abd9ca96629504b36b82f7f61
Satya Sharma <satyasharma2964@gmail.com>
2021-01-30 17:48:11 +0530

    Merging other pages and adding bg music to Contact page

---
+++ contact_index.js
@@ -0,0 +1,81 @@
+
+window.addEventListener("DOMContentLoaded", event => {
+  const audio = document.querySelector("audio");
+  audio.volume = 0.2;
+  audio.play();
+});
+
+
+
+
+var delayInMilliseconds = 10000; //1 second
+
+setTimeout(function() {
+$("h2").text("to change the body background-color")
+}, delayInMilliseconds);
+
+$(".twitter").hover(function () {
+	$("#social-section").toggleClass("color-twitter")
+	$("h2").text("Twitter")
+	$("h1").css("color", "Black");
+  setTimeout()
+
+});
+
+$(".facebook").hover(function () {
+	$("#social-section").toggleClass("color-facebook")
+	$("h2").text("Facebook")
+	$("h1").css("color", "Black");
+  setTimeout()
+
+});
+
+$(".googleplus").hover(function () {
+	$("#social-section").toggleClass("color-googleplus")
+	$("h2").text("Google+")
+	$("h1").css("color", "Black");
+	setTimeout()
+
+
+});
+$(".github").hover(function () {
+	$("#social-section").toggleClass("color-github")
+	$("h2").text("GitHub")
+	 $("h1").css("color", "white");
+		setTimeout()
+
+});
+$(".pinterest").hover(function () {
+	$("#social-section").toggleClass("color-pinterest")
+	$("h2").text("Pinterest")
+	$("h1").css("color", "Black");
+	setTimeout()
+
+
+});
+
+$(".dribbble").hover(function () {
+	$("#social-section").toggleClass("color-dribbble")
+	$("h2").text("Dribble")
+	$("h1").css("color", "Black");
+	setTimeout()
+
+
+});
+
+$(".instagram").hover(function () {
+	$("#social-section").toggleClass("color-instagram")
+	$("h2").text("Instagram")
+	$("h1").css("color", "Black");
+	setTimeout()
+
+
+});
+$(".linkedin").hover(function () {
+	$("#social-section").toggleClass("color-linkedin")
+	$("h2").text("linkedin")
+	$("h1").css("color", "Black");
+	setTimeout()
+
+
+});
