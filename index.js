85f73ec49094c533c5969266bbfa7d7e3a259228
Satya Sharma <satyasharma2964@gmail.com>
2021-01-31 13:35:27 +0530

    Index page sound and bg changed

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 window.addEventListener("DOMContentLoaded", event => {
-  var audio=new Audio("Audio/note.mp3");
+  var audio=new Audio("Audio/samidare.mp3");
   audio.volume = 0.2;
   audio.play();
 });
